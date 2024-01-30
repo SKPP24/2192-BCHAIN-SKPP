d692cd36d22f2c9624a42924f1fd3a6bc0cc5c37
SKPP24 <98503719+SKPP24@users.noreply.github.com>
2024-01-30 15:00:49 +0800

    Add files via upload

---
+++ Prelimexam/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import React from 'react';
+import BibleQuiz from './components/biblequiz';
+
+function App() {
+  return (
+    <div>
+      <BibleQuiz />
+    </div>
+  );
+}
+
+export default App;
+
+
